55a9356446d90746c1bd32069fc0eb45e9e2fece
tommyprand <tommy.prando@gmail.com>
2021-07-31 10:28:38 +0000

    Sistemati services

---
+++ src/main/java/com/mallorcaWorks/service/GrandMasterService.java
@@ -0,0 +1,43 @@
+package com.mallorcaWorks.service;
+
+import com.mallorcaWorks.model.GrandMaster;
+import com.mallorcaWorks.model.Student;
+import com.mallorcaWorks.repository.GrandMasterRepo;
+import com.mallorcaWorks.repository.StudentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GrandMasterService {
+
+    @Autowired
+    private GrandMasterRepo repository;
+
+    public void save(GrandMaster obj) {
+        repository.save(obj);
+    }
+
+    public void saveAll(List<GrandMaster> list){
+        repository.saveAll(list);
+    }
+
+    public List<GrandMaster> getAll() {
+        return (List<GrandMaster>) repository.findAll();
+    }
+
+    public GrandMaster getById(int id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public void delete(int id) {
+        repository.deleteById(id);
+    }
+
+    public void updateUser(GrandMaster user) {
+        GrandMaster user1 = repository.findById(user.getId()).orElse(null);
+        // TODO
+    }
+
+}
